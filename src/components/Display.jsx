68d7177385927e731823fbe66cca87b2d3e62852
unknown <luigimachado111u@gmail.com>
2023-10-27 15:28:45 -0300

    Fesature-1

---
+++ src/components/Display.jsx
@@ -0,0 +1,5 @@
+import React, { Component } from "react"
+import './Display.css' // importando css
+
+export default props => 
+    <div className="display">{props.value}</div>
--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Calculator from "./main/Calculator";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div className="corpo">
+      <table>
+        <tr>
+          <td>
+            <h1>Calculadora</h1>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Calculator />
+          </td>
+        </tr>
+      </table>
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
